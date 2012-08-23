1a1c6c1772aed9e93c5d10dd95ae957261faad56
Eleven Concepts <travis@elevenconcepts.com>
2012-08-23 00:12:16 -0500

    Adding in the controller and view for the exception handler

---
+++ application/controllers/ErrorController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This class provides the structure for providing error reporting
+ * @package Framsie
+ * @subpackage ErrorController
+ * @version 1.0
+ * @copyright 2012 Travis Brown <tmbrown6@gmail.com>
+ * @license GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
+ * @author Travis Brown <tmbrown6@gmail.com>
+ */
+class ErrorController extends FramsieController {
+
+	///////////////////////////////////////////////////////////////////////////
+	/// View Methods /////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * This view action simply displays the error
+	 * @package Framsie
+	 * @subpackage ErrorController
+	 * @access public
+	 * @return void
+	 */
+	public function defaultView() {
+		// Set the page title
+		$this->setPageTitle('Error!');
+	}
+
+}
