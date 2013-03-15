cb3e39b162e9d7f666548b71bb65251c3d45b7e9
Travis Brown <tmbrown6@gmail.com>
2013-03-15 16:26:04 -0300

    Updating the automatic value generation

--- lib/framsie/TableMapper.php
+++ lib/framsie/TableMapper.php
@@ -247,7 +247,19 @@ class FramsieTableMapper {
 			// Determine if the default value should be anything special
 			switch ($oColumn->Default) {
 				// Timestamp
-				case 'CURRENT_TIMESTAMP' : $sDefaultValue = date('Y-m-d H:i:s'); break;
+				case 'CURRENT_TIMESTAMP' : 
+					// Check for a set timezone
+					if (ini_get('date.timezone')) {
+						// Create the datetime object
+						$oDate = new DateTime('now', new DateTimeZone(ini_get('date.timezone')));
+						// Set the default value
+						$sDefaultValue = $oDate->format('Y-m-d H:i:s');
+					} else {
+						// Use the system default
+						$sDefaultValue = date('Y-m-d H:i:s');
+					}
+				// We're done
+				break;
 				// Everything else
 				default                  : $sDefaultValue = $oColumn->Default;   break;
 			}
@@ -298,6 +310,16 @@ class FramsieTableMapper {
 			return (integer) $mValue;
 		}
 		if (preg_match(self::PATTERN_STRING,  $this->mColumns[$sColumn]->Type)) { // String
+			// Check to see if this is a DateTime object
+			if ($oDate = new DateTime($mValue)) {
+				// Check for a timezone
+				if (ini_get('date.timezone')) {
+					// Set the timezone
+					$oDate->setTimezone(ini_get('date.timezone'));
+				}
+				// Return the date value
+				return (string) $oDate->format('Y-m-d H:i:s');
+			}
 			// Return the value
 			return (string)  $mValue;
 		}
