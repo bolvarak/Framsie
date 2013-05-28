9fc85988c3a8ddd50de1f4ee6a4f38ab49ee687a
Travis Brown <tmbrown6@gmail.com>
2013-05-28 14:25:03 -0400

    Updating Framsie

--- public/index.php
+++ public/index.php
@@ -3,10 +3,6 @@
 require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'Framsie.php');
 // Initialize the system
 Framsie::Bootstrap();
-// Initialize the error class
-FramsieError::InitializeErrorsFromDatabase('ErrorLookup', 'Code', 'Message');
-// Add a redirect
-Framsie::getInstance()->addRedirect('/home/faq', '/home/faqs');
 // Initialize Caching
 FramsieCache::getInstance()->setExpire(1);
 // Dispatch the system
